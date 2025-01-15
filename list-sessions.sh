e59d413ceac6e234f74693c285c520a1159a622a
Zeyu Huang <zeyu.huang@gatech.edu>
2025-01-15 16:37:36 -0500

    update

--- list-sessions.sh
+++ list-sessions.sh
@@ -4,9 +4,9 @@ curl -G --data-urlencode "query=WITH
 e AS (SELECT *, row_number() OVER (ORDER BY timestamp DESC) FROM controls WHERE on_off = false),
 s AS (SELECT * FROM controls WHERE on_off = true),
 r AS (SELECT e.timestamp AS et, s.timestamp AS st, FROM e ASOF JOIN s WHERE e.row_number is not NULL)
-SELECT COUNT(sensors.timestamp), st FROM
+SELECT COUNT(sensors.timestamp), et FROM
   sensors JOIN r ON sensors.timestamp >= r.st AND sensors.timestamp <= r.et
-GROUP BY st
-order by st DESC;;" http://localhost:9000/exec
+GROUP BY et
+order by et DESC;;" http://localhost:9000/exec
 
 echo ""
