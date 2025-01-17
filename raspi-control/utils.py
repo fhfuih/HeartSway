f12eeb0855f52ec2f88a1433ddc4d9166169036e
Huang, Zeyu <fhfuih@outlook.com>
2025-01-16 19:05:26 -0500

    bugfix: trough.mean

--- raspi-control/utils.py
+++ raspi-control/utils.py
@@ -99,7 +99,7 @@ class SensorDataController:
         for i, (p1, p2) in enumerate(zip(peaks_idx[:-1], peaks_idx[1:])):
             t = troughs_idx[np.logical_and(troughs_idx > p1, troughs_idx < p2)]
             if t.size > 1:
-                t = np.mean(t, dtype=int)
+                t = np.mean(t, dtype=int)[0]
             # The first half is the inhale (ibi going down)
             # Inhale time = sum of ibi from the first peak to the trough
             try:
